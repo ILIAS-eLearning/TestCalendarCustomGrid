7fe77285714d4f808cc98bc744379a0be2aa0e56
xus <xus206@hotmail.com>
2017-08-14 13:59:49 +0200

    Edit agenda item + edit shy button title

--- classes/class.ilCustomGridPlugin.php
+++ classes/class.ilCustomGridPlugin.php
@@ -18,24 +18,31 @@ class ilCustomGridPlugin extends ilAppointmentCustomGridPlugin
 		return "CustomGrid";
 	}
 
+	/**
+	 * Replace the whole appointment presentation in the grid.
+	 * @return mixed string or empty.
+	 */
 	public function replaceContent()
 	{
 		//return "<p style='font-size:90%;'>[PLUGIN] Content replaced.</p>";
 		return;
 	}
 
+	/**
+	 * @return string or empty.
+	 */
 	public function addExtraContent()
 	{
 		return "<p style='font-size:90%;'>[PLUGIN] Content added.</p>";
 	}
 
-	//TODO this method should return only the path.
+	//this method should return only the path.
 	public function addGlyph()
 	{
 		$appointment = $this->getAppointment();
 		$icon = false;
 
-		$cat_id = ilCalendarCategoryAssignments::_lookupCategory($appointment['event']->getEntryId());
+		$cat_id = ilCalendarCategoryAssignments::_lookupCategory($appointment->getEntryId());
 		$cat = ilCalendarCategory::getInstanceByCategoryId($cat_id);
 		$cat_info["type"] = $cat->getType();
 		$cat_info["obj_id"] = $cat->getObjId();
@@ -67,14 +74,46 @@ class ilCustomGridPlugin extends ilAppointmentCustomGridPlugin
 
 	}
 
-	public function replaceTitle()
+	//Agenda Methods
+	/**
+	 * @return \ILIAS\UI\Component\Item\Item $a_item
+	 */
+	public function editAgendaItem($shy, $properties, $color)
 	{
-		return "[Plugin] Title changed by plugin";
+		global $DIC;
+
+		$f = $DIC->ui()->factory();
+		$df = new \ILIAS\Data\Factory();
+
+		//example dealing with calendar types.
+		$cat_id = ilCalendarCategoryAssignments::_lookupCategory($this->appointment->getEntryId());
+		$cat = ilCalendarCategory::getInstanceByCategoryId($cat_id);
+		if(ilObject::_lookupType($cat->getObjId()) == "sess") {
+			$description_color = "blue";
+		}
+		else {
+			$description_color = "orange";
+		}
+		//example dealing with appointment properties.
+		if($this->appointment->isFullday()) {
+			$description = "<span style='color:".$description_color."'>[PLUGIN] This appointment is FULL DAY   - ".$this->appointment->getDescription()."</span>";
+			$properties["metadata by PLUGIN"] = "<h3>This text exists because the plugin knows that this is a full day event</h3>";
+		} else {
+			$description = "<span style='color:$description_color'>[PLUGIN] This appointment is NOT full day  - ".$this->appointment->getDescription()."</span>";
+		}
+
+		$li = $f->item()->standard($shy)
+			->withDescription("".$description)
+			->withLeadText(ilDatePresentation::formatPeriod($this->start_date, $this->appointment->getEnd(), true))
+			->withProperties($properties)
+			->withColor($df->color('#'.$color));
+
+		return $li;
 	}
 
-	public  function replaceDescription()
+	public function editShyButtonTitle()
 	{
-		return "[Plugin] Description changed by plugin";
+		return "[PLUGIN]changed this title.";
 	}
 
 }
